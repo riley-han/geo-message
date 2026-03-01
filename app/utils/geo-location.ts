57148630fdf79bb18d077d2766c87a508a6c96e7
Riley Hansen <rileyhansen29@gmail.com>
2026-03-01 12:15:41 -0800

    some message data and ui

---
+++ app/utils/geo-location.ts
@@ -0,0 +1,26 @@
+
+
+export const GEOFENCE_RADIUS_METERS = 80;
+
+const toRad = (value: number) => (value * Math.PI) / 180;
+
+export const getDistanceMeters = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) => {
+  const R = 6371000;
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) ** 2 +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) ** 2;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
+};
